3f85f0354b5663296d9b01892053b6bea682b3e2
clairemacdonald <claire.macdonald@blis.com>
2018-09-06 10:37:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks(){
 fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => {console.log(json)});
+  .then(json => {console.log(json)
+    
+  });
 }
 
   //write fetch request to the Game of Thrones API

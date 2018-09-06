7f732c938eee31980713883dd9876bd54787eb70
clairemacdonald <claire.macdonald@blis.com>
2018-09-06 11:20:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function fetchBooks(){
 fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => {console.log(json)
-    renderBooks(json)
+  .then(json => {console.log(json);
+    renderBooks(json);
   });
 }
 
@@ -10,14 +10,14 @@ fetch('https://anapioficeandfire.com/api/books')
 
 
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('main');
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});

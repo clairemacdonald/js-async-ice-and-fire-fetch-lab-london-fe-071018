e8c065955120d4315df107b31bd1717f1f05ffe4
clairemacdonald <claire.macdonald@blis.com>
2018-09-06 10:27:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function fetchBooks(){
-  fetch(url) 
+fetch(url) // Call the fetch function passing the url of the API as a parameter
 .then(function() {
-    
+    // Your code for handling the data you get from the API
 })
 .catch(function() {
-    
+    // This is where you run code if the server returns any errors
 });
 }
 
